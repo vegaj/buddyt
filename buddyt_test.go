7641dc79492c8d3ccf123800c3d0c6ad8a02d717
vegaj <jvegamoreno@outlook.com>
2017-05-24 12:45:50 +0200

    UT1.05
    Cambiado archivos

--- buddyt_test.go
+++ buddyt_test.go
@@ -1,10 +1,10 @@
 package calcTest
 
 import "testing"
+import "github.com/vegaj/buddyt"
 
 func TestDivisionZero(t testing.T) {
-
-	res, err := Div(3,0)
+	res, err := buddyt.Div(3,0)
 	if (err != nil) {
 		t.FailNow()
 	}
